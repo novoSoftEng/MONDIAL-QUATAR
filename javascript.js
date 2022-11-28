c9c611772e243a1aa74a77ab565a0cf01c5da3b0
chihab liefrid <hellsecr2@gmail.com>
2022-11-28 23:46:46 +0100

    initial iframe to div

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 
-let mountains = [
-  { name: "Monte Falco", height: 1658},
+/*let mountains = [
+  { name: "Monte Falco", height: 1658 },
   { name: "Monte Falterona", height: 1654 },
-  { name: "Poggio Scali", height: 1520},
-  { name: "Pratomagno", height: 1592},
-  { name: "Monte Amiata", height: 1738}
+  { name: "Poggio Scali", height: 1520 },
+  { name: "Pratomagno", height: 1592 },
+  { name: "Monte Amiata", height: 1738 }
 ];
 function generateTableHead(table, data) {
   let thead = table.createTHead();
@@ -16,9 +16,9 @@ function generateTableHead(table, data) {
     row.appendChild(th);
   }
   let th = document.createElement("th");
-    let text = document.createTextNode("modifier");
-    th.appendChild(text);
-    row.appendChild(th);
+  let text = document.createTextNode("modifier");
+  th.appendChild(text);
+  row.appendChild(th);
 }
 
 function generateTable(table, data) {
@@ -40,4 +40,30 @@ function generateTable(table, data) {
 let table = document.querySelector("table");
 let data = Object.keys(mountains[0]);
 generateTable(table, mountains);
-generateTableHead(table, data);
+generateTableHead(table, data);*/
+//actualite
+
+var frm = ['v3', 'v4'];
+var hrf = [['./images/a1.jpg', 'a1.html'], ['./images/a2.jpg', 'a1.html'], ['./images/a3.jpg', 'a1.html']];
+function page_a(index) {
+let a_p1 = ['La France est la première équipe à se qualifier pour les huitièmes de finale de la Coupe du monde de football au Qatar.', 'Les Bleus se sont imposés 2 buts à 1 face au Danemark ce samedi, grâce à un doublé de Kyllian Mbappé.', 'Dans l\'autre match du groupe D, l\'Australie l\'a emporté 1 à 0 contre la Tunisie. Le but de Mitchell Duke permet aux Socceroos de se relancer dans la course à la qualification, avant un dernier match décisif face aux Danois.', 'Dans le groupe C, l\'Argentine respire. Après une défaite surprise face à l\'Arabie saoudite, l\'Albiceleste a battu le Mexique 2 à 0 avec un but de Lionel Messi, son deuxième du tournoi.', 'Les grands joueurs ont décidément été décisifs ce samedi. L\'attaquant polonais Robert Lewandowski a permis à son équipe de s\'imposer 2 à 0 contre l\'Arabie saoudite avec un but et une passe décisive, alors que les Saoudiens ont manqué un penalty.'];
+  
+  for (let i = 0, l = a_p1.length; i < l; i++) {
+    let page_a = document.getElementById("page_a");
+
+    let br = document.createElement("br");
+    let p = document.createElement("p");
+    p.innerText = a_p1[i];
+    page_a.appendChild(p);
+    page_a.appendChild(br);
+
+  }
+}
+function fon_a(index) {
+  for (i = 0, l = frm.length; i < l; i++) {
+    document.querySelector('iframe[name="' + frm[i] + '"]').src = hrf[index][i];
+  }
+  page_a(index);
+}
+
+
