fa2d4839f27d5c396a2a0330aebdf2cd6166fc21
chihab liefrid <hellsecr2@gmail.com>
2022-11-29 10:03:39 +0100

    working div 4

--- javascript.js
+++ javascript.js
@@ -42,15 +42,12 @@ let data = Object.keys(mountains[0]);
 generateTable(table, mountains);
 generateTableHead(table, data);*/
 //actualite
-
-var frm = ['v3', 'v4'];
-var hrf = [['./images/a1.jpg', 'a1.html'], ['./images/a2.jpg', 'a1.html'], ['./images/a3.jpg', 'a1.html']];
-function page_a(index) {
+var hrf = ['./images/a1.jpg','./images/a2.jpg','./images/a3.jpg'];
+function page_a() {
 let a_p1 = ['La France est la première équipe à se qualifier pour les huitièmes de finale de la Coupe du monde de football au Qatar.', 'Les Bleus se sont imposés 2 buts à 1 face au Danemark ce samedi, grâce à un doublé de Kyllian Mbappé.', 'Dans l\'autre match du groupe D, l\'Australie l\'a emporté 1 à 0 contre la Tunisie. Le but de Mitchell Duke permet aux Socceroos de se relancer dans la course à la qualification, avant un dernier match décisif face aux Danois.', 'Dans le groupe C, l\'Argentine respire. Après une défaite surprise face à l\'Arabie saoudite, l\'Albiceleste a battu le Mexique 2 à 0 avec un but de Lionel Messi, son deuxième du tournoi.', 'Les grands joueurs ont décidément été décisifs ce samedi. L\'attaquant polonais Robert Lewandowski a permis à son équipe de s\'imposer 2 à 0 contre l\'Arabie saoudite avec un but et une passe décisive, alors que les Saoudiens ont manqué un penalty.'];
   
   for (let i = 0, l = a_p1.length; i < l; i++) {
     let page_a = document.getElementById("page_a");
-
     let br = document.createElement("br");
     let p = document.createElement("p");
     p.innerText = a_p1[i];
@@ -59,11 +56,9 @@ let a_p1 = ['La France est la première équipe à se qualifier pour les huitiè
 
   }
 }
-function fon_a(index) {
-  for (i = 0, l = frm.length; i < l; i++) {
-    document.querySelector('iframe[name="' + frm[i] + '"]').src = hrf[index][i];
-  }
-  page_a(index);
+function fon_a(num) {
+  document.querySelector('iframe[name="v3"]').src = hrf[num];
+  page_a();
 }
 
 
