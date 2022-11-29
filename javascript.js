3380eb1a01b4ad24fdd9ed252b54f0c1d06e4769
youssef <youssef.achmalchetouan@etu.uae.ac.ma>
2022-11-29 16:57:13 +0100

    termination d ajouter et supprimer

--- javascript.js
+++ javascript.js
@@ -16,6 +16,7 @@ function generateTableHead(table, data) {
     let text = document.createTextNode(key);
     th.appendChild(text);
     row.appendChild(th);
+
   }
   let th = document.createElement("th");
   let th1 = document.createElement("th");
@@ -36,6 +37,7 @@ function generateTable(table, data) {
       let cell = row.insertCell();
       let text = document.createTextNode(element[key]);
       cell.appendChild(text);
+
     }
     var btn1 = document.createElement("input");
     btn1.type = "button";
@@ -43,7 +45,14 @@ function generateTable(table, data) {
     btn1.className = "bt1";
     let cell = row.insertCell();
     cell.append(btn1);
-
+    btn1.onclick = function () {
+      var i = row.rowIndex;
+      let n1 = String(prompt("entrer le nom"));
+       let n2 = String(prompt("entrer la description"));
+       table.rows[i].cells[0].innerText=n1;
+       table.rows[i].cells[1].innerText=n2;
+      
+    }
     var btn2 = document.createElement("input");
     btn2.type = "button";
     btn2.value = "suprimer";
@@ -51,64 +60,99 @@ function generateTable(table, data) {
 
     let cell1 = row.insertCell();
     cell1.append(btn2);
-    btn2.onclick=function(){
-      var i=row.rowIndex;
+    btn2.onclick = function () {
+      var i = row.rowIndex;
       table.deleteRow(i);
     }
   }
+}
+
+function fc1() {
+  var txt = document.createElement("input");
+  txt.className = "t1";
+  txt.type = "text";
+  txt.placeholder = "entrer le joueur";
+  var input = document.createElement("input");
+  input.type = "button";
+  input.value = "ajouter";
+  input.className = "t2";
+
+  document.getElementById("d1").appendChild(txt);
+  document.getElementById("d1").appendChild(input);
+  input.addEventListener("click", (event) => {
+    let row1 = table.insertRow();
+    let cell1 = row1.insertCell();
+    let cell2 = row1.insertCell();
+    let cell3 = row1.insertCell();
+    let cell4 = row1.insertCell();
+    let contenu = document.createTextNode(txt.value);
+    cell1.appendChild(contenu);
+    let n2 = String(prompt("entrer la description"));
+    let contenu2 = document.createTextNode(n2);
+    cell2.appendChild(contenu2);
+    var btn3 = document.createElement("input");
+    btn3.type = "button";
+    btn3.value = "modifier";
+    btn3.className = "bt1";
+
+
+
+    var btn4 = document.createElement("input");
+    btn4.type = "button";
+    btn4.value = "suprimer";
+    btn4.className = "bt1";
+
+    cell3.append(btn3);
+    btn3.onclick = function () {
+      var i = row1.rowIndex;
+      let n1 = String(prompt("entrer le nom"));
+       let n2 = String(prompt("entrer la description"));
+       table.rows[i].cells[0].innerText=n1;
+       table.rows[i].cells[1].innerText=n2;
+      
+    }
+    cell4.append(btn4);
+
+    btn4.onclick = function () {
+      var i = row1.rowIndex;
+      table.deleteRow(i);
+    }
+  });
+
+
 }
 
 let table = document.querySelector("table");
 let data = Object.keys(mountains[0]);
 generateTable(table, mountains);
 generateTableHead(table, data);
-function fc1(){
-var txt = document.createElement("input");
-txt.className = "t1";
-txt.type = "text";
-txt.placeholder = "entrer le joueur";
-var input = document.createElement("input");
-input.type = "button";
-input.value = "ajouter";
-input.className = "t2";
+//actualite
 
-document.getElementById("d1").appendChild(txt);
-document.getElementById("d1").appendChild(input);
-input.addEventListener("click", (event) => {
-  let row1 = table.insertRow();
-  let cell1 = row1.insertCell();
-  let cell2 = row1.insertCell();
-  let cell3 = row1.insertCell();
-  let cell4 = row1.insertCell();
-  let contenu = document.createTextNode(txt.value);
-  cell1.appendChild(contenu);
-  let n2 = String(prompt("entrer la description"));
-  let contenu2 = document.createTextNode(n2);
-  cell2.appendChild(contenu2);
-  var btn3 = document.createElement("input");
-  btn3.type = "button";
-  btn3.value = "modifier";
-  btn3.className = "bt1";
-    
+function page_a(num) {
+  let a_p = [['La France est la première équipe à se qualifier pour les huitièmes de finale de la Coupe du monde de football au Qatar.', 'Les Bleus se sont imposés 2 buts à 1 face au Danemark ce samedi, grâce à un doublé de Kyllian Mbappé.', 'Dans l\'autre match du groupe D, l\'Australie l\'a emporté 1 à 0 contre la Tunisie. Le but de Mitchell Duke permet aux Socceroos de se relancer dans la course à la qualification, avant un dernier match décisif face aux Danois.', 'Dans le groupe C, l\'Argentine respire. Après une défaite surprise face à l\'Arabie saoudite, l\'Albiceleste a battu le Mexique 2 à 0 avec un but de Lionel Messi, son deuxième du tournoi.',
+    'Les grands joueurs ont décidément été décisifs ce samedi. L\'attaquant polonais Robert Lewandowski a permis à son équipe de s\'imposer 2 à 0 contre l\'Arabie saoudite avec un but et une passe décisive, alors que les Saoudiens ont manqué un penalty.']
+    , ["L'un a le vent en poupe et l'étiquette de favori, l'autre est sans employeur et a souffert pour emmener son pays au Qatar : le Mondial 2022 accueille ce jeudi les débuts de deux stars planétaires, le Brésilien Neymar et le Portugais Ronaldo.",
+    "Après les rencontres du jour - Suisse-Cameroun (11h) et Brésil-Serbie (20h) dans le groupe G, Uruguay-Corée du Sud (14) et Portugal-Ghana (19h) dans le groupe H -, toutes les équipes auront disputé leur premier match.0"],
+  ["Cette deuxième journée du mondial de football au Qatar marque l'entrée en scène de l'Angleterre, qui affronte ce lundi l'Iran dans le groupe B.",
+    "La sélection anglaise devra faire sans son arrière latéral droit, Kyle Walker, blessé aux adducteurs. Le défenseur de Manchester City n'a pas joué depuis le 2 octobre. De son côté, l'Iran entre en compétition sans pression, mais avec un effectif amoindri à cause des blessures. "
+    ,
+    "Le milieu de terrain Omid Ebrahimi est forfait pour ce mondial, blessé à l'aine. L'attaquant du Bayer Leverkusen Sardar Azmoun fait quant à lui partie de l'effectif, malgré une déchirure au mollet le mois dernier."]];
+  let page_a = document.getElementById("page_a");
+  page_a.replaceChildren();
+  for (let i = 0, l = a_p[num].length; i < l; i++) {
+    let br = document.createElement("br");
+    let p = document.createElement("p");
+    p.innerText = a_p[num][i];
+    page_a.append(p);
+    page_a.append(br);
 
-
-  var btn4 = document.createElement("input");
-  btn4.type = "button";
-  btn4.value = "suprimer";
-  btn4.className = "bt1";
- 
-  cell3.append(btn3);
-  cell4.append(btn4);
-
-  btn4.onclick=function(){
-    var i=row1.rowIndex;
-    table.deleteRow(i);
   }
-});
-  
-
 }
-
+function fon_a(num) {
+  var hrf = ['./images/a1.jpg', './images/a2.jpg', './images/a3.jpg'];
+  document.querySelector('iframe[name="v3"]').src = hrf[num];
+  page_a(num);
+}
 
 
 
