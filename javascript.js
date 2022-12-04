30bf362eb593fe5aa48b4d78d9f72a27e701b70c
chihab liefrid <hellsecr2@gmail.com>
2022-12-04 12:41:02 +0100

    hhzef

--- javascript.js
+++ javascript.js
@@ -379,17 +379,8 @@ function map(v3) {
   img.src="./images/maroc.jpg"; 
   img.usemap="#imaage-map";
 v3.append(img);
-  let map=document.createElement("map") ;
-  map.name="imaage-map";
-  let area=document.createElement("area");
-      area.shape="rect" 
-      area.coords="484,147,519,196"; 
-      area.title="Yassine Bounou";
-      area.href="#";
       area.onclick=function () {
         creeTable(0);
       }
       map.appendChild(area);
-v3.append(map);
-v3.append(area);
 }
